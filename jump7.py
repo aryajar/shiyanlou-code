14690fbdacfe1f91948a6dbea2bd62add9ffcb9d
aryajar <wangshengnan@stockhn.name>
2020-07-22 17:28:13 +0800

    the practice

---
+++ jump7.py
@@ -0,0 +1,11 @@
+set a repositories
+Linux: cd /bendi
+git clone [repositories\'s url]
+cd /the url
+input content
+git add --all
+git config --global user.email "wangshengnan@stockhn.com"
+git config --global user.name "aryajar"
+git commit -m 'commit'
+git remote add origin url
+git push origin master
